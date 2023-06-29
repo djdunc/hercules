d99ae327fb326efa8828b5fb90efa0b879b6be3b
Duncan Wilson <drdunc@gmail.com>
2023-06-29 18:05:28 +0100

    test script for mmWave sensor

--- COACH/mmWave-GPIO/mmWave-GPIO.ino
+++ COACH/mmWave-GPIO/mmWave-GPIO.ino
@@ -1,17 +1,51 @@
+#include <WiFiNINA.h>           // wifi library for the MKR1010
+#include <utility/wifi_drv.h>   // library to drive to RGB LED on the MKR1010
+
 #define PresencePin  5
+
+int presence = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(PresencePin, INPUT);  
 
+  // RGB LED's
+  WiFiDrv::pinMode(25, OUTPUT); // G
+  WiFiDrv::pinMode(26, OUTPUT); // R
+  WiFiDrv::pinMode(27, OUTPUT); // B  
+
+  blinkRGB(100,10,0,0,200);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  presence = digitalRead(PresencePin);
 
   Serial.print("Presence State: ");
-  Serial.println(digitalRead(PresencePin));
+  Serial.println(presence);
+  if(presence){
+    blinkRGB(100,10,0,200,0);
+  } else {
+    blinkRGB(100,10,200,0,0);
+  }
 
-  delay(1000);
+  //delay(1000);
+}
+
+
+void blinkRGB(int ms, int loops, int r, int g, int b){
+  while(loops){
+    WiFiDrv::analogWrite(25, r);
+    WiFiDrv::analogWrite(26, g);
+    WiFiDrv::analogWrite(27, b);
+    delay(ms);
+    WiFiDrv::analogWrite(25, 0);
+    WiFiDrv::analogWrite(26, 0);
+    WiFiDrv::analogWrite(27, 0);
+    delay(ms);
+    loops-=1;   
+  }
+
 }
