2a36c4330bdb42cd97597a1c1a3962167df17e51
Duncan Wilson <drdunc@gmail.com>
2023-06-29 18:07:23 +0100

    testing combination of sensors that were conflicting

--- COACH/coach_HDC_SEN0390/coach_HDC_SEN0390.ino
+++ COACH/coach_HDC_SEN0390/coach_HDC_SEN0390.ino
@@ -8,6 +8,8 @@
 #include "Adafruit_HDC1000.h"
 DFRobot_B_LUX_V30B    myLux(13,7,8);//The sensor chip is set to 13 pins, SCL and SDA adopt default configuration
 Adafruit_HDC1000 hdc = Adafruit_HDC1000();
+#define CO2SensorPin A2           // this pin read the analog voltage from the sound level meter
+float ppm = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -28,5 +30,32 @@ void loop() {
   Serial.print(hdc.readTemperature());
   Serial.print(" - Humidity: ");
   Serial.println(hdc.readHumidity());
+
+  // CO2
+  int sensorValue = analogRead(CO2SensorPin);
+  float voltage = sensorValue*(3300/1023.0);
+  if(voltage == 0)
+  {
+    Serial.println("Fault");
+  }
+  else if(voltage < 400)
+  {
+    Serial.println("preheating");
+  }
+  else
+  {
+    int voltage_difference=voltage-400;
+    ppm=voltage_difference*50.0/16.0;
+
+    Serial.print("voltage: ");
+    Serial.print(voltage);
+    Serial.print("mv - ");
+    //Print CO2 concentration
+    Serial.print("CO2 Concentration: ");
+    Serial.print(ppm);
+    Serial.println("ppm");
+
+  }  
+
   delay(1000);
 }
